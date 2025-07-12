9228fe06175e150942e09b4995b51f1386ed5aea
rohantandale1107 <rohanhtandale11@gmail.com>
2025-07-12 18:58:00 +0530

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Login from './components/Auth/Login'
 
 const App = () => {
   return (
     <>
-      <Login/>
+      <Login />
     </>
   )
 }
---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        // Handle login logic here
+        console.log("email is", email);
+        console.log("password is", password);
+
+        setEmail('');
+        setPassword('');
+    }
+
+
+
+    return (
+        <div className="flex h-screen w-screen items-center justify-center bg-neutral-950">
+            <div className="w-full max-w-sm p-8 bg-transparent rounded-xl shadow-lg border border-gray-200">
+                <h2 className="text-2xl font-semibold text-center mb-6 text-white">Login</h2>
+                <form onSubmit={(e)=>{
+                    submitHandler(e);
+                    e.preventDefault();
+                }} className="flex flex-col space-y-4">
+                    <input
+                    value={email}
+                        onChange={(e)=>{
+                            setEmail(e.target.value);
+                        }}
+                        type="text"
+                        required
+                        placeholder="Enter email"
+                        className="border border-gray-300 rounded-lg px-5 py-3 focus:outline-none focus:ring-2 focus:ring-emerald-400"
+                    />
+                    <input
+                        value={password}
+                        onChange={(e)=>{
+                            setPassword(e.target.value);
+                        }}
+                        type="password"
+                        required
+                        placeholder="Enter password"
+                        className="border border-gray-300 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-emerald-400"
+                    />
+                    <button
+                        type="submit"
+                        className="bg-red-400 hover:bg-red-500 text-white font-semibold py-3 rounded-lg transition"
+                    >
+                        Login
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
