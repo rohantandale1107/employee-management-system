04c8f68658b8bfcdea6a9248732300518d2a50ea
rohantandale1107 <rohanhtandale11@gmail.com>
2025-07-14 00:04:30 +0530

    task list

---
+++ src/components/Others/AllTask.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const AllTask = () => {
+    return (
+        <div className='mt-5 bg-[#1C1C1C] mt-5 h-48 overflow-auto p-5 rounded mt-5'>
+            <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded '>
+                <h2>rohan</h2>
+                <h3>make a ui design</h3>
+                <h5>status</h5>
+            </div>
+            <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded '>
+                <h2>rohan</h2>
+                <h3>make a ui design</h3>
+                <h5>status</h5>
+            </div>
+            <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded '>
+                <h2>rohan</h2>
+                <h3>make a ui design</h3>
+                <h5>status</h5>
+            </div>
+            <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded '>
+                <h2>rohan</h2>
+                <h3>make a ui design</h3>
+                <h5>status</h5>
+            </div>
+            <div className='bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded '>
+                <h2>rohan</h2>
+                <h3>make a ui design</h3>
+                <h5>status</h5>
+            </div>
+
+        </div>
+    )
+}
+
+export default AllTask
