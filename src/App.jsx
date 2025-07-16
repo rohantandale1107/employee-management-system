bf1599d1e31a5773f7d54faea5d0cc0cf40df4b0
rohantandale1107 <rohanhtandale11@gmail.com>
2025-07-17 04:09:35 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import TaskList from './components/Tasklist/TaskList'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { getLocalStorage, setLocalStorage } from './utils/localStorage';
+import { AuthContext } from './context/AuthProvider'
 
 
 const App = () => {
@@ -15,16 +16,24 @@ const App = () => {
 
   // }, )
 
+
+
+
   const [user, setUser] = useState(null)
+  const authData = useContext(AuthContext);
+ 
+
   const handleLogin = (email, password) => {
-    if(email=='admin@me.com' && password=='123'){
-      console.log('this is admin')
+    if (email == 'admin@me.com' && password == '123') {
+      setUser('admin')
+
     }
-    else if(email == 'user@me.com'&& password == '123'){
-      console.log('this is user')
+    else if ( authData &&  authData.employees.find((e)=> email == e.email && password == e.password)) {
+      setUser('employee')
+
     }
-    else{
-        alert('Invalid credentials')
+    else {
+      alert('Invalid credentials')
     }
   }
 
@@ -32,12 +41,8 @@ const App = () => {
 
   return (
     <>
-      {!user ? <Login handleLogin={handleLogin}/> : ''}
-
-
-      
-      <EmployeeDashboard/> 
-      <AdminDashboard/>
+      {!user ? <Login handleLogin={handleLogin} /> : ''}
+      {user == 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   )
 }
